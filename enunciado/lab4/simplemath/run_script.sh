4de6c14e76e33c0822a06ddd9b3ba8f9bd252efa
Bernardo do Amaral Teodosio <berteodosio@gmail.com>
2019-10-17 18:25:01 -0300

    improve run script

--- enunciado/lab4/simplemath/run_script.sh
+++ enunciado/lab4/simplemath/run_script.sh
@@ -1,4 +1,50 @@
 #!/bin/bash
 
+echo "TEST 0"
+./script.sh tests/test0.sm
+./printer
+
+echo ""
+echo "TEST 1"
+./script.sh tests/test1.sm
+./printer
+
+echo ""
+echo "TEST 2"
+./script.sh tests/test2.sm
+./printer
+
+echo ""
+echo "TEST 3"
+./script.sh tests/test3.sm
+./printer
+
+echo ""
+echo "TEST 4"
+./script.sh tests/test4.sm
+./printer
+
+echo ""
+echo "TEST 5"
+./script.sh tests/test5.sm
+./printer
+
+echo ""
+echo "TEST 6"
+./script.sh tests/test6.sm
+./printer
+
+echo ""
+echo "TEST 7"
 ./script.sh tests/test7.sm
+./printer
+
+echo ""
+echo "TEST 8"
+./script.sh tests/test8.sm
+./printer
+
+echo ""
+echo "TEST 9"
+./script.sh tests/test9.sm
 ./printer
